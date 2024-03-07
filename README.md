c25239b71527b947735ee77950e7d724d79d3180
fb0801 <fbhatti0801@gmail.com>
2024-03-07 18:13:54 +0000

    setup 4

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ crud app built using Node, Express and MongoDB
 ## Resources used:
 
 https://www.toptal.com/developers/gitignore/
+
+https://www.npmjs.com/package/express
+
+https://www.mongodb.com/
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose');
 
 app.listen(3000, () => {
     console.log("running")
@@ -9,3 +10,5 @@ app.listen(3000, () => {
 app.get("/", (req, res) => {
     res.send("hello ")
 })
+
+mongoose.connect("mongodb+srv://f:<password>@backenddb.ff3rrfz.mongodb.net/n?retryWrites=true&w=majority&appName=BackendDB")
