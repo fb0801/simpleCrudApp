646b9099d839d97904229a7cc5e441a498c73308
fb0801 <fbhatti0801@gmail.com>
2024-03-08 17:58:01 +0000

    api requests 2

--- index.js
+++ index.js
@@ -17,8 +17,20 @@ app.get("/", (req, res) => {
 app.get("/api/products", async(req,res) => {
 
     try{
+      const products =  await Product.find({})
+      res.status(200).json(products)
 
     }catch(error){
+        res.status(500).json({message:error.message})
+    }
+})
+
+app.get("/api/product/:id", async (req, res) => {
+    try {
+            const {id} = req.params
+            const product = await Product.findById(id)
+            res.status(200).json(product)
+    }catch (error){
         res.status(500).json({message:error.message})
     }
 })
