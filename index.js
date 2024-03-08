60af9a771d974a2f1ef9f66774d00844a54d8189
fb0801 <fbhatti0801@gmail.com>
2024-03-08 18:21:36 +0000

    api requests refactor

--- index.js
+++ index.js
@@ -8,74 +8,14 @@ const Product = require('./models/product.model.js')
 
 app.use(express.json())
 
-
+//route
+app.use("./api/products", productRoute)
 
 app.get("/", (req, res) => {
     res.send("hello from API ")
 })
 
-app.get("/api/products", async(req,res) => {
 
-    try{
-      const products =  await Product.find({})
-      res.status(200).json(products)
-
-    }catch(error){
-        res.status(500).json({message:error.message})
-    }
-})
-
-app.get("/api/product/:id", async (req, res) => {
-    try {
-            const {id} = req.params
-            const product = await Product.findById(id)
-            res.status(200).json(product)
-    }catch (error){
-        res.status(500).json({message:error.message})
-    }
-})
-
-app.post('/api/products', async (req, res) => {
-    try {
-        const product = await Product.create(res.body)
-        res.status(200).json(product)
-        
-    }catch(error){
-        res.status(500).json({message: error.message})
-    }
-})
-
-//update product
-app.put('/api/product/:id', async (req,res) => {
-    try{
-        const {id} = req.params
-        const product = await Product.findByIdAndUpdate(id, req.body)
-        if (!product){
-            return res.status(404).json({message: "product not found"})
-        }
-            const updatedProduct = await Product.findById(id)
-            res.status(200).json(updatedProduct)
-    }catch (error){
-        res.status(500).json({message: error.message})
-
-    }
-})
-
-//delete
-app.delete('/api/product/:id', async (res, req) => {
-    try{
-        const {id} = req.params
-        const product = await Product.findByIdAndDelete(id)
-
-        if (!product){
-            return res.status(404).json({message: "product not found"})
-        }
-        res.status(404).json({message: 'product removed'})
-
-    }catch (error){
-        res.status(500).json({message: error.message})
-    }
-})
 
 
 mongoose.connect("mongodb+srv://$:$@backenddb.ff3rrfz.mongodb.net/nodeapi?retryWrites=true&w=majority&appName=BackendDB")
